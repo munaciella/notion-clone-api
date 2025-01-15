459bbf9636eb8a182618070a2a1fb7ea9fcde830
munaciella <franes88@gmail.com>
2025-01-15 15:17:29 +0000

    run new deploy

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,74 @@
-import OpenAI from "openai";
-import { Hono } from "hono";
-import { cors } from "hono/cors";
+import OpenAI from 'openai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
 type Bindings = {
-  OPENAI_API_KEY: string;
-  AI: Ai;
+	OPEN_AI_KEY: string;
+	AI: Ai;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.use(
-  '/*',
-  cors({
-    origin: '*',
-    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
-    allowMethods: ['GET', 'POST', 'PUT', 'OPTIONS'],
-    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
-    maxAge: 600,
-    credentials: true,
-  })
+	'/*',
+	cors({
+		origin: '*',
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+		allowMethods: ['GET', 'POST', 'PUT', 'OPTIONS'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
 );
 
-// Default route for debugging
 app.get('/', (c) => {
-  return c.text('Hono backend is up and running!');
+	return c.text('Hono backend is up and running!');
+});
+
+app.post('/chatToDocument', async (c) => {
+	const openai = new OpenAI({
+		apiKey: c.env.OPEN_AI_KEY,
+	});
+
+	const { documentData, question } = await c.req.json();
+
+	const chatCompletion = await openai.chat.completions.create({
+		messages: [
+			{
+				role: 'system',
+				content:
+				'You are an assistant helping the user to chat to a document, I am providing a JSON file of the markdown for the document. Using this, answer the user question in the clearest way possible. The document is about: ' +
+				documentData,
+			},
+			{
+				role: 'user',
+				content: 'My question is: ' + question,
+			},
+		],
+		model: 'gpt-3.5-turbo',
+		temperature: 0.5,
+	});
+
+	const response = chatCompletion.choices[0]?.message?.content;
+
+	return c.json({ message: response });
 });
 
 app.post('/translateDocument', async (c) => {
-  const { documentData, targetLang } = await c.req.json();
+	const { documentData, targetLang } = await c.req.json();
 
-  const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
-    input_text: documentData,
-    max_length: 1000,
-  });
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
+		input_text: documentData,
+		max_length: 1000,
+	});
 
-  const response = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
-    text: summaryResponse.summary,
-    source_lang: 'english',
-    target_lang: targetLang,
-  });
+	const response = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summaryResponse.summary,
+		source_lang: 'english',
+		target_lang: targetLang,
+	});
 
-  return new Response(JSON.stringify(response));
+	return new Response(JSON.stringify(response));
 });
 
 export default app;
